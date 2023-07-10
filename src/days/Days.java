b1e3cbec030d6970cd8e4c57b0516189eb0a0c68
Paarth001 <iamperry002@sheridancollege.ca>
2023-07-10 17:32:44 -0400

    mid term local repository

--- src/days/Days.java
+++ src/days/Days.java
@@ -24,7 +24,7 @@ public class Days {
     public static void main(String[] args) {
          
     Scanner in =new Scanner(System.in);
-    System.out.println("Enter the day number (one,two,three,four,five) in string   ");
+    System.out.println("Enter the day number (one,two,three,four,five) in string");
     String code = in.next();
     Weekdays t= new Weekdays();
     t.nameOfDay(code); 
